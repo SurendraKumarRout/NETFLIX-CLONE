7f62b07287fbe5956075d9dc591f57f03731eef0
SurendraKumarRout <surendra.igit@gmail.com>
2022-03-07 00:44:03 +0530

    Banner done

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import { requests } from "./requests.js";
 import {Row} from "./components/Row.jsx"
 import { Helmet } from "react-helmet";
 import Banner from "./components/Banner.jsx";
+import Navbar from "./components/Navbar.jsx"
 
 function App() {
   return (
     <div className="App">
       <Helmet>
-        <style>{"body { background-color: #282c34; }"}</style>
+        <style>{"body { background-color:  #1a2228; }"}</style>
       </Helmet>
-      <Banner/>
-      <Row title="Netflix Orignals" fetchUrl={requests.fetchNetflixOrignals} isLarge/>
+      <Navbar />
+      <Banner />
+      <Row
+        title="Netflix Orignals"
+        fetchUrl={requests.fetchNetflixOrignals}
+        isLarge
+      />
       <Row title="Trending" fetchUrl={requests.fetchTrending} />
       <Row title="Top-Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Rommance" fetchUrl={requests.fetchRomanticMovies} />
       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-      <Row title="Upcoming" fetchUrl={requests.fetchUpcoming} />
+      {/* <Row title="Upcoming" fetchUrl={requests.fetchUpcoming} /> */}
     </div>
   );
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { instance } from "../axios";
 import { requests } from "../requests";
+import { HEADER } from "./banner.js";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -10,22 +11,48 @@ function Banner() {
   useEffect(() => {
     async function fetchData() {
       const request = await instance.get(requests.fetchNetflixOrignals);
-      setMovie(Math.floor(Math.random() * (request.data.results.length - 1)));
-
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
       return request;
     }
     fetchData();
-  }, []);
-  // console.log(movie)
+  }, [] );
     
-    return (
-        <header className="banner">
-            <div className="banner_content">
+    console.log( movie )
+    
+    // FUNCTION FOR LIMITING THE WORDS IN DESCRIPTION
+    function truncate ( str, n )
+    {
+        return str?.length > n ? str.substr( 0, n - 1 ) + "..." : str;
+    }
 
-            </div>
-            
-        </header>
-    );
+  return (
+    <HEADER
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+                    "https://image.tmdb.org/t/p/original/${movie.backdrop_path}"
+                )`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+      </div>
+      <div className="banner_gradient"></div>
+    </HEADER>
+  );
 }
 
 export default Banner;
