ab4a4aa5b5e9e650eea138180b14b28f033390f3
SurendraKumarRout <surendra.igit@gmail.com>
2022-03-12 21:10:45 +0530

    banner bg position changed to top

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -69,7 +69,7 @@ function Banner() {
           backgroundImage: `url(
                     "https://image.tmdb.org/t/p/original/${movie.backdrop_path}"
                 )`,
-          backgroundPosition: "center center",
+          backgroundPosition: "top",
         }}
       >
         <div className="banner_contents">
